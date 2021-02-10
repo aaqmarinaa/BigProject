fb1ef892e0cd8272e0240632a3765a0700d6636d
Almas Aqmarina <almasaqmarina@gmail.com>
2021-02-10 21:36:17 +0700

    add leave tc

--- src/main/java/com/sekolahqa/pages/Leave.java
+++ src/main/java/com/sekolahqa/pages/Leave.java
@@ -1,18 +1,41 @@
 package com.sekolahqa.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Leave extends BasePage {
     private By btnLeave = new By.ByXPath("//*[@id=\"menu_leave_viewLeaveModule\"]");
     private By btnAssignLeave = new By.ByXPath("//*[@id=\"menu_leave_assignLeave\"]");
+    private By btnApplyLeave = new By.ByXPath("//*[@id=\"menu_leave_applyLeave\"]");
     private By fieldEmployeeName = new By.ByXPath("//*[@id=\"assignleave_txtEmployee_empName\"]");
-    private By selectLeaveType = new By.ByXPath("//*[@id=\"assignleave_txtLeaveType\"]");
+    private By selectAssignLeaveType = new By.ByXPath("//*[@id=\"assignleave_txtLeaveType\"]");
+
+    private By fromDateAssignleave = new By.ByXPath("//*[@id=\"assignleave_txtFromDate\"]");
+    private By toDateAssignLeave = new By.ByXPath("//*[@id=\"applyleave_txtToDate\"]");
+
+    private By fromDateApplyleave = new By.ByXPath("//*[@id=\"applyleave_txtFromDate\"]");
+    private By toDateApplyLeave = new By.ByXPath("//*[@id=\"applyleave_txtToDate\"]");
+
+    private By btnAssignleave = new By.ByXPath("//*[@id=\"assignBtn\"]");
+    private By menuLeaveList = new By.ByXPath("//*[@id=\"menu_leave_viewLeaveList\"]");
+
+    private By toDateLabelAssign = new By.ByXPath("//*[@id=\"frmLeaveApply\"]/fieldset/ol/li[5]/label");
+    private By toDateLabelApply = new By.ByXPath("//*[@id=\"frmLeaveApply\"]/fieldset/ol/li[4]/label");
+
+    private By btnSubmitApply = new By.ByXPath("//*[@id=\"applyBtn\"]");
+    private By menuApplyLeave = new By.ByXPath("//*[@id=\"menu_leave_applyLeave\"]");
+    private By selectApplyLeaveType = new By.ByXPath("//*[@id=\"applyleave_txtLeaveType\"]");
 
     public Leave(WebDriver driver) { super(driver); }
 
     public void clickMenuLeave() throws InterruptedException {
         clickButton(btnLeave);
+    }
+
+    public void clickApplyLeave() throws InterruptedException {
+        clickButton(menuApplyLeave);
     }
 
     public void clickAssignLeave() throws InterruptedException {
@@ -23,7 +46,38 @@ public class Leave extends BasePage {
         inputText(fieldEmployeeName, employeeName);
     }
 
-    public void setSelectLeaveType(String leaveType) throws InterruptedException {
-        inputText(selectLeaveType, leaveType);
+    public void setSelectAssignLeaveType(String leaveType) throws InterruptedException {
+        inputText(selectAssignLeaveType, leaveType);
+    }
+
+    public void setSelectApplyLeaveType(String leaveType) throws InterruptedException {
+        inputText(selectApplyLeaveType, leaveType);
+    }
+
+    public void inputAssignFromDateLeave(String fromDate) throws InterruptedException {
+        clickButton(fromDateAssignleave);
+        inputText(fromDateAssignleave, fromDate);
+    }
+
+    public void inputAssignToDateLeave(String toDate) throws InterruptedException {
+        clickButton(toDateLabelAssign);
+        inputText(toDateAssignLeave, toDate);
+    }
+
+    public void inputApplyFromDateLeave(String fromDate) throws InterruptedException {
+        clickButton(fromDateApplyleave);
+        inputText(fromDateApplyleave, fromDate);
+    }
+    public void inputApplyToDateLeave(String toDate) throws InterruptedException {
+        clickButton(toDateLabelApply);
+        inputText(toDateApplyLeave, toDate);
+    }
+
+    public void clickAssignButton() throws InterruptedException {
+        clickButton(btnAssignLeave);
+    }
+
+    public void clickApplyButton() throws InterruptedException {
+        clickButton(btnSubmitApply);
     }
 }
--- src/test/java/com/orangehrm/LeaveTest.java
+++ src/test/java/com/orangehrm/LeaveTest.java
@@ -4,7 +4,9 @@ import com.sekolahqa.config.Utils;
 import com.sekolahqa.pages.Leave;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.io.IOException;
 
@@ -16,15 +18,33 @@ public class LeaveTest {
     public LeaveTest() throws IOException {
     }
 
-    // Admin assignee leave for employee
+    // Admin assignee leave for employee from admin
     @Test
     public void assigneeLeaveForEmployee() throws InterruptedException {
         loginTest.LoginWithValidFormatAdmin();
         leave.clickMenuLeave();
         leave.clickAssignLeave();
+        Thread.sleep(300);
         leave.inputEmployeeName("Almas Aqmarina");
-        leave.setSelectLeaveType("Cuti");
+        Thread.sleep(300);
+        leave.setSelectAssignLeaveType("Cuti");
+        Thread.sleep(300);
+        leave.inputAssignFromDateLeave("2021-03-22");
+        Thread.sleep(300);
+        leave.inputAssignToDateLeave("2021-03-23");
+        Thread.sleep(300);
+        leave.clickAssignButton();
     }
 
-//    @Test
+    // Employee apply leave
+    @Test
+    public void applyLeaveFromEmployee() throws InterruptedException {
+        loginTest.LoginWithEmployeeAccount();
+        leave.clickMenuLeave();
+        leave.clickApplyLeave();
+        leave.setSelectApplyLeaveType("Cuti");
+        leave.inputApplyFromDateLeave("2021-04-15");
+        leave.inputApplyToDateLeave("2021-04-16");
+        leave.clickApplyButton();
+    }
 }
